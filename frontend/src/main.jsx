4efd95f5f276aab42b2dd5acd6afcdac27aa9c2f
Srinivas Senapati <srinivassenapati5624@gmail.com>
2026-04-27 01:33:05 +0530

    ..

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,5 @@
-import { StrictMode }    from "react";
-import { createRoot }    from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "./context/ThemeContext";
 import "./index.css";
 import App from "./App";
